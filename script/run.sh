ffb7254b7412cb2e1ef0310245be2652ae46cc54
iranildobatalha <iranildobatalha@gmail.com>
2020-01-07 11:43:05 -0400

    Arquivos de configuração

--- script/run.sh
+++ script/run.sh
@@ -11,7 +11,7 @@ mkdir -p $baseline
 run_dir=$baseline/$run/
 mkdir -p ${run_dir}
 
-config="{\"seed\":"$run", \"batch_size\": 256, \"model_type\": \"l2ac\", \"top_n\": "${top_n}", \"ncls\": "$ncls",\"set_mode\": \"train1\", \"db\": \"amazon\", \"out_dir\": \""${run_dir}"\", \"set_modes\": [\"test_25\", \"test_50\", \"test_75\"], \"vote_n\": 1}"
+config="{\"seed\":"$run", \"batch_size\": 256, \"model_type\": \"l2ac\", \"top_n\": "${top_n}", \"ncls\": "$ncls",\"set_mode\": \"train1\", \"db\": \"amazon\", \"out_dir\": \""${run_dir}"\", \"set_modes\": [\"test_50\", \"test_75\"], \"vote_n\": 1}"
 config_file=$run_dir/config.json
 echo -e $config > ${config_file}
 python ../src/train_l2ac.py ${config_file}
